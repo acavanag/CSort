4f56277855494496c1e553f6b6427b905c6246d2
acavanaghww <acavanagh@weddingwire.com>
2014-06-09 11:25:48 -0700

    commit

--- CSort/main.m
+++ CSort/main.m
@@ -8,10 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+int fastSort(const void *a, const void *b)
+{
+    int intA = *((int*)a);
+    int intB = *((int*)b);
+    
+    if ( intA == intB ) return 0;
+    else if ( intA < intB ) return -1;
+    else return 1;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSLog(@"building array...");
+        
+        int a[1000000];
+        
+        for (int i = 0; i < 1000000; i++)
+        {
+            a[i] = rand();
+        }
+        
+        NSLog(@"sorting....");
+        
+        double start = CFAbsoluteTimeGetCurrent();
+        qsort( a, 1000000, sizeof(int), fastSort);
+        double end = CFAbsoluteTimeGetCurrent();
+        
+        NSLog(@"time: %f", end-start);
+        
     }
     return 0;
 }
